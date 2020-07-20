15bbc630137b6d82837e5e0c2e167d15130323ef
Maximilian Köstler <maximilian@koestler.hamburg>
2020-07-20 08:58:37 +0200

    Support identifiers in lexer

--- backend/src/formula/lexer.rs
+++ backend/src/formula/lexer.rs
@@ -62,22 +62,49 @@ struct Lexer {
     tokens: Vec<Token>,
 }
 
-fn get_number(it: &mut str::Chars) -> f64 {
-    let str_slice = it.as_str();
+trait PrefixTakable<'a> {
+    fn take_prefix<P>(self: &mut Self, predicate: P) -> &'a str
+    where
+        P: Fn(&char) -> bool;
+}
 
-    let numeric_chars = it
-        .clone()
-        .take_while(|c| c.is_ascii_digit() || *c == '.')
-        .count();
+impl<'a> PrefixTakable<'a> for str::Chars<'a> {
+    fn take_prefix<P>(self: &mut Self, predicate: P) -> &'a str
+    where
+        P: Fn(&char) -> bool,
+    {
+        // This function is O(n), but takes 3 passes over the matching part of the iterator
+        // 1st pass: find the end of the matching prefix
+        // 2nd pass: forward the iterator to the last character of the prefix
+        // 3rd pass: extract the string slice (indexing utf8-slices is O(n))
 
-    // upon returning, `it` must point to the last numeric char
-    // `it + numeric_chars` points at the first non-numeric or at the end of the string
-    // `it + numeric_chars - 1` points at the last numeric char (where we need to go)
-    if numeric_chars > 1 {
-        it.nth(numeric_chars - 1 - 1); // -1 because nth(x) advances x + 1 times
+        let str_slice = self.as_str();
+
+        let (count, end) = self
+            .clone()
+            .take_while(predicate)
+            .map(|c| c.len_utf8())
+            .fold((0, 0), |acc, x| (acc.0 + 1, acc.1 + x));
+
+        // upon returning, `it` must point to the char matching the predicate
+        // `it + count` points at the first non-matching or at the end of the string
+        // `it + count - 1` points at the last matching (where we need to go)
+        if count > 1 {
+            self.nth(count - 1 - 1); // -1 because nth(x) advances x + 1 times
+        }
+
+        &str_slice[..end]
     }
+}
 
-    str_slice[..numeric_chars].parse().unwrap()
+fn get_number(it: &mut str::Chars) -> f64 {
+    it.take_prefix(|c| c.is_ascii_digit() || *c == '.')
+        .parse()
+        .unwrap()
+}
+
+fn get_identifier<'a>(it: &'a mut str::Chars) -> &'a str {
+    it.take_prefix(|c| c.is_alphanumeric())
 }
 
 impl Lexer {
@@ -90,6 +117,10 @@ impl Lexer {
                 '0'..='9' => {
                     let value = get_number(&mut it);
                     tokens.push(Token::Number(value));
+                }
+                x if x.is_alphabetic() => {
+                    let value = get_identifier(&mut it);
+                    tokens.push(Token::Identifier(value.to_owned()));
                 }
                 '+' => {
                     tokens.push(Token::Operator(Operator::Add));
@@ -109,7 +140,7 @@ impl Lexer {
                 ')' => {
                     tokens.push(Token::Bracket(Bracket::RoundClose));
                 }
-                ' ' => {}
+                x if x.is_whitespace() => {}
                 _ => {
                     return Err(format!("Unexpected symbol: {}", c));
                 }
@@ -144,6 +175,16 @@ mod tests {
         .boxed()
     }
 
+    fn identifier_strategy() -> BoxedStrategy<String> {
+        proptest::string::string_regex(r"\p{Alphabetic}[\p{Alphabetic}\d]*")
+            .unwrap()
+            .boxed()
+    }
+
+    fn whitespace_strategy() -> BoxedStrategy<String> {
+        proptest::string::string_regex(r"\s*").unwrap().boxed()
+    }
+
     #[test]
     fn tokenize_invalid() {
         let lexer = Lexer::new("$");
@@ -152,8 +193,8 @@ mod tests {
 
     proptest! {
         #[test]
-        fn tokenize_value(value: f64) {
-            let lexer = Lexer::new(&value.to_string()).unwrap();
+        fn tokenize_value(value: f64, spaces in whitespace_strategy()) {
+            let lexer = Lexer::new(&format!("{}{}", value, spaces)).unwrap();
 
             let mut expected = vec![];
             if value < 0.0 {
@@ -161,6 +202,15 @@ mod tests {
             }
             expected.push(Token::Number(value.abs()));
             prop_assert_eq!(&lexer[..], &expected[..]);
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn tokenize_identifier(value in identifier_strategy(), spaces in whitespace_strategy()) {
+            let lexer = Lexer::new(&format!("{}{}", value, spaces)).unwrap();
+            let expected = vec![Token::Identifier(value)];
+            assert_eq!(&lexer[..], &expected[..]);
         }
     }
 
@@ -198,10 +248,11 @@ mod tests {
 
     #[test]
     fn tokenize_complex_expression() {
-        let lexer = Lexer::new("0+ (1.5 + 10* -200)/3").unwrap();
+        let lexer = Lexer::new("0+ fn3(1.5 + 10* -200)/3/你好").unwrap();
         let expected = vec![
             Token::Number(0.0),
             Token::Operator(Operator::Add),
+            Token::Identifier("fn3".to_owned()),
             Token::Bracket(Bracket::RoundOpen),
             Token::Number(1.5),
             Token::Operator(Operator::Add),
@@ -212,6 +263,8 @@ mod tests {
             Token::Bracket(Bracket::RoundClose),
             Token::Operator(Operator::Div),
             Token::Number(3.0),
+            Token::Operator(Operator::Div),
+            Token::Identifier("你好".to_owned()),
         ];
         assert_eq!(&lexer[..], &expected[..]);
     }
