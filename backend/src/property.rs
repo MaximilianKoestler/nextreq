acf66a168ba384b8bcdf951d9207c8e812c397cc
Maximilian Köstler <maximilian@koestler.hamburg>
2020-07-22 16:14:34 +0200

    Start work on table properties

--- backend/src/property.rs
+++ backend/src/property.rs
@@ -5,12 +5,14 @@ use crate::formula::{error::FormulaError, Formula};
 #[derive(Debug, Clone)]
 pub enum PropertyError {
     FormulaError(FormulaError),
+    NoMatchError(String),
 }
 
 impl fmt::Display for PropertyError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::FormulaError(err) => write!(f, "{}", err),
+            Self::NoMatchError(err) => write!(f, "{}", err),
         }
     }
 }
@@ -68,6 +70,53 @@ impl Property for FormulaProperty {
     }
 }
 
+pub enum Match {
+    Equal(f64),
+    Range(f64, f64),
+    Any,
+}
+
+pub struct TableProperty {
+    input: Box<dyn Property>,
+    matches: Vec<(Match, Box<dyn Property>)>,
+}
+
+impl TableProperty {
+    fn new(input: Box<dyn Property>, matches: Vec<(Match, Box<dyn Property>)>) -> Self {
+        Self {
+            input: input,
+            matches: matches,
+        }
+    }
+}
+
+impl Property for TableProperty {
+    fn value(&self) -> Result<f64, PropertyError> {
+        let value = self.input.value()?;
+        for (m, prop) in &self.matches {
+            match m {
+                Match::Equal(v) if value == *v => {
+                    return prop.value();
+                }
+                Match::Range(min, max) if value >= *min && value <= *max => {
+                    return prop.value();
+                }
+                Match::Any => {
+                    return prop.value();
+                }
+                _ => {}
+            }
+        }
+        Err(PropertyError::NoMatchError(format!(
+            "value {} could not be matched",
+            value
+        )))
+    }
+    fn unit(&self) -> Option<&str> {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -91,7 +140,7 @@ mod tests {
     }
 
     #[test]
-    fn formula_addition() {
+    fn formula_property_addition() {
         let p = FormulaProperty::new("1 + 2", Some("m")).unwrap();
         assert_eq!(p.value().unwrap(), 3.0);
     }
@@ -102,12 +151,77 @@ mod tests {
             .. ProptestConfig::default()
         })]
         #[test]
-        fn formula_does_not_crash(input: String) {
+        fn formula_property_does_not_crash(input: String) {
             let p = FormulaProperty::new(&input, Some("m"));
 
             if p.is_ok() {
                 let _ = p.unwrap().value();
             }
         }
+    }
+
+    #[test]
+    fn table_property_no_match() {
+        let input = ValueProperty::new(1.0, Some("m"));
+        let matches = vec![(
+            Match::Equal(2.0),
+            Box::new(ValueProperty::new(4.0, Some("m"))) as Box<dyn Property>,
+        )];
+        let p = TableProperty::new(Box::new(input), matches);
+        assert!(p.value().is_err());
+    }
+
+    #[test]
+    fn table_property_equals() {
+        let input = ValueProperty::new(2.0, Some("m"));
+        let matches = vec![(
+            Match::Equal(2.0),
+            Box::new(ValueProperty::new(4.0, Some("m"))) as Box<dyn Property>,
+        )];
+        let p = TableProperty::new(Box::new(input), matches);
+        assert_eq!(p.value().unwrap(), 4.0);
+    }
+
+    #[test]
+    fn table_property_range() {
+        let input = ValueProperty::new(2.5, Some("m"));
+        let matches = vec![(
+            Match::Range(2.0, 3.0),
+            Box::new(ValueProperty::new(4.0, Some("m"))) as Box<dyn Property>,
+        )];
+        let p = TableProperty::new(Box::new(input), matches);
+        assert_eq!(p.value().unwrap(), 4.0);
+    }
+
+    #[test]
+    fn table_property_any() {
+        let input = ValueProperty::new(0.0, Some("m"));
+        let matches = vec![(
+            Match::Any,
+            Box::new(ValueProperty::new(4.0, Some("m"))) as Box<dyn Property>,
+        )];
+        let p = TableProperty::new(Box::new(input), matches);
+        assert_eq!(p.value().unwrap(), 4.0);
+    }
+
+    #[test]
+    fn table_property_complex() {
+        let input = FormulaProperty::new("3!", Some("m")).unwrap();
+        let matches = vec![
+            (
+                Match::Equal(1.0),
+                Box::new(ValueProperty::new(4.0, Some("m"))) as Box<dyn Property>,
+            ),
+            (
+                Match::Range(2.0, 7.0),
+                Box::new(FormulaProperty::new("5 + 1", Some("m")).unwrap()) as Box<dyn Property>,
+            ),
+            (
+                Match::Any,
+                Box::new(ValueProperty::new(2.0, Some("m"))) as Box<dyn Property>,
+            ),
+        ];
+        let p = TableProperty::new(Box::new(input), matches);
+        assert_eq!(p.value().unwrap(), 6.0);
     }
 }
