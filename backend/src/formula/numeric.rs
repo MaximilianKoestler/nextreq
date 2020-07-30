ac93d80e1ba19c631674c20d8333aced76dd75dd
Maximilian Köstler <maximilian@koestler.hamburg>
2020-07-30 09:45:13 +0200

    Use BigDecimal instead of float

--- backend/src/formula/numeric.rs
+++ backend/src/formula/numeric.rs
@@ -1,8 +1,11 @@
 use std::{cmp, fmt, ops, str};
 
+use bigdecimal::{BigDecimal, FromPrimitive, One, ParseBigDecimalError, ToPrimitive};
+use num::pow::pow;
+
 use super::error::FormulaError;
 
-pub type ParseError = std::num::ParseFloatError;
+pub type ParseError = ParseBigDecimalError;
 
 macro_rules! build_err {
     ($($arg:tt)*) => {{
@@ -18,7 +21,7 @@ macro_rules! error {
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Numeric {
-    value: f64,
+    value: BigDecimal,
 }
 
 impl Numeric {
@@ -30,45 +33,87 @@ impl Numeric {
 
     pub fn trunc(&self) -> Self {
         Self {
-            value: self.value.trunc(),
+            value: self.value.with_scale(0),
         }
     }
 
     pub fn round(&self, precision: &Self) -> Result<Self, FormulaError> {
-        let factor = 10.0f64.powf(precision.value.trunc());
-        let rounded = (self.value * factor.clone()).round() / factor;
+        let precision = precision
+            .value
+            .to_usize()
+            .ok_or(build_err!("precision is out of range"))?;
+        if precision > 100 {
+            error!("precision > 100 is not supported");
+        }
+
+        let scale = pow(BigDecimal::from(10), precision + 1);
+        let pivot = BigDecimal::from(5) / scale;
+        let pivoted = if self.value >= BigDecimal::from(0) {
+            self.value.clone() + pivot
+        } else {
+            self.value.clone() - pivot
+        };
+        let rounded = pivoted.with_scale(precision as i64);
         Ok(Self { value: rounded })
     }
 
     pub fn sqrt(&self) -> Result<Self, FormulaError> {
-        if self.value < 0.0 {
-            error!("sqrt of negative numbers is not supported")
-        }
-        Ok(Self {
-            value: self.value.sqrt(),
-        })
+        let sqrt = self
+            .value
+            .sqrt()
+            .ok_or(build_err!("sqrt of negative numbers is not supported"))?;
+
+        Ok(Self { value: sqrt })
     }
 
     pub fn pow(&self, rhs: &Self) -> Result<Self, FormulaError> {
+        if rhs.abs().value > BigDecimal::from(100) {
+            error!("exponents of magnitude > 100 are not supported");
+        }
+
+        let power = pow(
+            self.value.clone(),
+            rhs.value
+                .abs()
+                .to_usize()
+                .ok_or(build_err!("the exponent is out of range"))?,
+        );
+
         Ok(Self {
-            value: self.value.powf(rhs.value),
+            value: if power == BigDecimal::from(0) || rhs.value >= BigDecimal::from(0) {
+                power
+            } else {
+                BigDecimal::one() / power
+            },
         })
     }
 
     pub fn factorial(&self) -> Result<Self, FormulaError> {
-        // all factorials larger than `170!` will overflow an `f64`
-        let factorial = match self.value {
-            v if v < 0.0 => std::f64::NAN,
-            v if v > 170.0 => std::f64::INFINITY,
-            v => (1..=(v as u32)).fold(1.0, |a, b| a * b as f64),
-        };
+        if self.value > BigDecimal::from(100) {
+            error!("factorial of values > 100 are not supported");
+        }
+
+        let max_index = self
+            .value
+            .to_usize()
+            .ok_or(build_err!("factorial argument is out of range"))?;
+        let factorial =
+            (1..=max_index).fold(One::one(), |a, b| a * BigDecimal::from_usize(b).unwrap());
+
         Ok(Self { value: factorial })
     }
 }
 
 impl fmt::Display for Numeric {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.value)
+        // remove trailing decimal zeroes
+        let representation = self.value.to_string();
+        let representation = if representation.contains('.') {
+            representation.trim_end_matches('0').trim_end_matches('.')
+        } else {
+            &representation
+        };
+        write!(f, "{}", representation)
     }
 }
 
@@ -83,31 +128,34 @@ impl str::FromStr for Numeric {
 // only used temporarily while migrating to `Numeric` in favor of `f64`
 impl From<&Numeric> for f64 {
     fn from(numeric: &Numeric) -> Self {
-        numeric.value
+        numeric.value.to_f64().unwrap()
     }
 }
 
 impl From<Numeric> for usize {
     fn from(numeric: Numeric) -> Self {
-        numeric.value as usize
+        numeric.value.to_usize().unwrap_or(std::usize::MAX)
     }
 }
 
 impl From<f64> for Numeric {
     fn from(value: f64) -> Self {
-        Self { value }
+        Self {
+            value: BigDecimal::from_f64(value).unwrap(),
+        }
     }
 }
 
 impl cmp::PartialEq<f64> for Numeric {
     fn eq(&self, other: &f64) -> bool {
-        self.value.eq(other)
+        self.value.eq(&BigDecimal::from_f64(other.clone()).unwrap())
     }
 }
 
 impl cmp::PartialOrd<f64> for Numeric {
     fn partial_cmp(&self, other: &f64) -> Option<cmp::Ordering> {
-        self.value.partial_cmp(other)
+        self.value
+            .partial_cmp(&BigDecimal::from_f64(other.clone()).unwrap())
     }
 }
 
@@ -141,6 +189,9 @@ impl ops::Mul for Numeric {
 impl ops::Div for Numeric {
     type Output = Result<Self, FormulaError>;
     fn div(self, rhs: Self) -> Self::Output {
+        if rhs.value == BigDecimal::from(0) {
+            error!("division by zero is not allowed");
+        }
         Ok(Self {
             value: self.value / rhs.value,
         })
@@ -161,14 +212,16 @@ mod tests {
 
     impl From<&f64> for Numeric {
         fn from(value: &f64) -> Self {
-            Self { value: *value }
+            Self {
+                value: BigDecimal::from_f64(value.clone()).unwrap(),
+            }
         }
     }
 
     impl From<u32> for Numeric {
         fn from(value: u32) -> Self {
             Self {
-                value: value as f64,
+                value: BigDecimal::from_u32(value).unwrap(),
             }
         }
     }
