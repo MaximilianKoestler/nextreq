6c1a18f39ba0cb797801a80ed70fc5bd709cb742
Maximilian Köstler <maximilian@koestler.hamburg>
2020-08-09 13:19:40 +0200

    Add stub for calculator component

---
+++ frontend/src/components/Calculator.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+export default class Calculator extends Vue {}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ frontend/src/views/Calculator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Calculator />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Calculator from "@/components/Calculator.vue";
+
+@Options({
+  components: {
+    Calculator,
+  },
+})
+export default class Home extends Vue {}
+</script>
