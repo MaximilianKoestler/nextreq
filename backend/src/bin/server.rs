bfeb192ddeb892aeeb59f6d96e1187357f77e2fd
Maximilian Köstler <maximilian@koestler.hamburg>
2020-08-11 09:26:31 +0200

    Use rocket_cors instead of custom fairing

--- backend/src/bin/server.rs
+++ backend/src/bin/server.rs
@@ -1,10 +1,8 @@
 use juniper_rocket_async::{graphiql_source, GraphQLRequest, GraphQLResponse};
-use rocket::http::{self, Header, Method};
-use rocket::{
-    fairing::{Fairing, Info, Kind},
-    response::content,
-    Request, Response, State,
-};
+use rocket::http::Method;
+use rocket::{response::content, State};
+use rocket_cors::{self, AllowedHeaders, AllowedOrigins};
+use std::error::Error;
 
 use nextreq::graphql_frontend::{create_schema, Context, Schema};
 use nextreq::interactors::CalculatorInteractor;
@@ -12,15 +10,6 @@ use nextreq::interactors::CalculatorInteractor;
 #[rocket::get("/")]
 fn graphiql() -> content::Html<String> {
     graphiql_source("/graphql")
-}
-
-#[rocket::options("/graphql")]
-fn cors_preflight_handler<'a>() -> Response<'a> {
-    Response::build()
-        .raw_header("Access-Control-Allow-Origin", "*")
-        .raw_header("Access-Control-Allow-Methods", "OPTIONS, POST")
-        .raw_header("Access-Control-Allow-Headers", "Content-Type")
-        .finalize()
 }
 
 #[rocket::post("/graphql", data = "<request>")]
@@ -32,39 +21,26 @@ fn post_graphql_handler(
     request.execute_sync(&schema, &context)
 }
 
-struct AllowOriginsFairing;
-
-#[rocket::async_trait]
-impl Fairing for AllowOriginsFairing {
-    fn info(&self) -> rocket::fairing::Info {
-        Info {
-            name: "Add Access-Control-Allow-Origin header",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, req: &'r Request<'_>, res: &mut Response<'r>) {
-        if req.method() == Method::Post {
-            res.adjoin_header(Header::new(
-                http::hyper::header::ACCESS_CONTROL_ALLOW_ORIGIN.as_str(),
-                "*",
-            ))
-        }
-    }
-}
-
 #[rocket::main]
-async fn main() -> Result<(), rocket::error::Error> {
+async fn main() -> Result<(), Box<dyn Error>> {
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins: AllowedOrigins::all(),
+        allowed_methods: vec![Method::Post].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::all(),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()?;
+
     rocket::ignite()
         .manage(create_context())
         .manage(create_schema())
-        .mount(
-            "/",
-            rocket::routes![graphiql, post_graphql_handler, cors_preflight_handler],
-        )
-        .attach(AllowOriginsFairing {})
+        .mount("/", rocket::routes![graphiql, post_graphql_handler])
+        .attach(cors)
         .launch()
-        .await
+        .await?;
+
+    Ok(())
 }
 
 fn create_context() -> Context {
